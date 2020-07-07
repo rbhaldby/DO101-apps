5299c5254a4286adff0f8066c52f0ef48f499ef8
haldenby <haldenby@ca.ibm.com>
2020-07-07 12:08:42 -0400

    added Greg

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/gregrozon', function (req, res) {
+  res.send('Hello Greg!!!\n\nThis is my first git and Openshift Deployment!!!!\n\nMe noob, so me impressed!!!!');
+});
+
 app.get('/danno', function (req, res) {
   res.send('Hello \'No Time For That\' Danno!\n');
 });
